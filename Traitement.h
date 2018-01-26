e4a99a38796dd7d07cc78ab3ca40444f942b17ca
Catalin Gangalic <gangalicatalin@gmail.com>
2018-01-26 16:14:25 +0100

    Create Traitement.h

---
+++ Traitement.h
@@ -0,0 +1,79 @@
+/*************************************************************************
+                           Traitement  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface de la classe <Traitement> (fichier Traitement.h) ----------------
+#if ! defined ( Traitement_H )
+#define Traitement_H
+
+//--------------------------------------------------- Interfaces utilisées
+#include <map>
+#include <vector>
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+struct Option {
+    bool typeOption [3] ={0,0,0};      //1ère case: -g, 2ème: -e, 3ème: -t
+    int heure;
+    string nomFichierDot;
+};
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Traitement>
+//
+//
+//------------------------------------------------------------------------
+
+class Traitement
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+    
+    void Traiter(int argc, char* argv[]);
+
+//-------------------------------------------- Constructeurs - destructeur
+
+    Traitement ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    ~Traitement ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+    void TraitementGeneral(string f);
+    
+    void CreerFichierDot();
+
+//----------------------------------------------------- Attributs protégés
+	map <pair<string,string>,int> trajets;
+    map <string,int> hits;
+    vector <pair<string, int>> top;
+    Option option;
+    bool erreur =false;
+};
+
+//-------------------------------- Autres définitions dépendantes de <Traitement>
+
+#endif // Traitement_H
+
