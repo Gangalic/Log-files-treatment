0a26b9bdb8ead767ed36ef7b855741e5d93ab99d
Catalin Gangalic <gangalicatalin@gmail.com>
2018-02-01 00:03:55 +0100

    Update Requete.h

--- Requete.h
+++ Requete.h
@@ -1,25 +1,21 @@
 /*************************************************************************
                            Requete  -  description
                              -------------------
-    début                : $DATE$
-    copyright            : (C) $YEAR$ par $AUTHOR$
-    e-mail               : $EMAIL$
+    début                : 26/01/2018
+    copyright            : (C) 2018 par B3332 (Duraffourg & Gangalic)
+    e-mail               : b3332@insa-lyon.fr
 *************************************************************************/
 
 //---------- Interface de la classe <Requete> (fichier Requete.h) ----------------
 #if ! defined ( Requete_H )
 #define Requete_H
 
-//--------------------------------------------------- Interfaces utilisées
-
-//------------------------------------------------------------- Constantes
-
-//------------------------------------------------------------------ Types
-
 //------------------------------------------------------------------------
 // Rôle de la classe <Requete>
-//
-//
+// Utilisee pour traiter la ligne de chaque requete et retrouver le parametres
+// necessaires pour le traitement d'apres
+// Peut etre extensible en ajoutant des autres attributs pour des autres
+// traitements plus specifiques
 //------------------------------------------------------------------------
 
 class Requete
@@ -28,53 +24,55 @@ class Requete
 
 public:
 //----------------------------------------------------- Méthodes publiques
-    // type Méthode ( liste des paramètres );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
     
     string ObtenirURLdepart();
+    // Mode d'emploi :
+    // Donne l'URL de depart en extern
     
     string ObtenirURLarrivee();
+    // Mode d'emploi :
+    // Donne l'URL d'arrivee en extern
     
     int ObtenirHeure();
+    // Mode d'emploi :
+    // Donne l'heure en extern
     
     int ObtenirStatut();
+    // Mode d'emploi :
+    // Donne le statut de la requete en extern
     
     string ObtenirExtension();
-
+    // Mode d'emploi :
+    // Donne l'extension du fichier d'arrivee en extern
 
 //-------------------------------------------- Constructeurs - destructeur
-    Requete ( const string line);
-    // Mode d'emploi (constructeur de copie) :
-    //
-    // Contrat :
-    //
+    Requete ( const string line );
+    // Mode d'emploi:
+    // Copie une ligne pour le traiter dedans
 
     Requete ( );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
 
-    virtual ~Requete ( );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
+    ~Requete ( );
 
 //------------------------------------------------------------------ PRIVE
 
 protected:
 //----------------------------------------------------- Méthodes protégées
-void trouverURLarivee( string requette );
-
-void trouverHeure( string temps );
-
-void trouverURLdepart( string URL );
-
-void trouverExtension( string requete );
+    void trouverURLarrivee( string requete );
+    // Mode d'emploi :
+    // Retrouve l'URL d'arrivee depuis la line envoyee du constructeur 
+    
+    void trouverHeure( string temps );
+    // Mode d'emploi :
+    // Retrouve l'heure de requete depuis la line du constructeur 
+    
+    void trouverURLdepart( string URL );
+    // Mode d'emploi :
+    // Retrouve l'URL de depart depuis la line du constructeur 
+    
+    void trouverExtension( string requete );
+    // Mode d'emploi :
+    // Retrouve l'extension du fichier d'arrivee depuis la line du constructeur 
 
 //----------------------------------------------------- Attributs protégés
 	string URLdepart;
@@ -83,8 +81,6 @@ void trouverExtension( string requete );
 	int statut;
 	string extension;
 };
-
-//-------------------------------- Autres définitions dépendantes de <Requete>
 
 #endif // Requete_H
 
