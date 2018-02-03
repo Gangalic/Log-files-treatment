11c37bb88e470255859a7d4e0a226fb597c46f4c
Catalin Gangalic <gangalicatalin@gmail.com>
2018-02-03 14:31:57 +0100

    Commented

--- Traitement.h
+++ Traitement.h
@@ -1,9 +1,9 @@
 /*************************************************************************
                            Traitement  -  description
                              -------------------
-    début                : $DATE$
-    copyright            : (C) $YEAR$ par $AUTHOR$
-    e-mail               : $EMAIL$
+    début                : 26/01/2018
+    copyright            : (C) 2018 par B3332 (Duraffourg & Gangalic)
+    e-mail               : b3332@insa-lyon.fr
 *************************************************************************/
 
 //---------- Interface de la classe <Traitement> (fichier Traitement.h) ----------------
@@ -14,8 +14,6 @@
 #include <map>
 #include <vector>
 
-//------------------------------------------------------------- Constantes
-
 //------------------------------------------------------------------ Types
 struct Option {
     bool typeOption [3] ={0,0,0};      //1ère case: -g, 2ème: -e, 3ème: -t
@@ -25,8 +23,10 @@ struct Option {
 
 //------------------------------------------------------------------------
 // Rôle de la classe <Traitement>
-//
-//
+// Utilisee pour effectuer la verification et traitement des valeurs et
+// instructions inserees par l'utilisateur
+// Interprete aussi les options et manipule les valeurs necessaires pour
+// trouver le top des pages accedees et creation du fichier .dot (si indique)
 //------------------------------------------------------------------------
 
 class Traitement
@@ -34,41 +34,33 @@ class Traitement
 //----------------------------------------------------------------- PUBLIC
 
 public:
-
-//------------------------------------------------------- Méthodes publics
+//----------------------------------------------------- Méthodes publiques
+    
     bool Traiter(int argc, char* argv[]);
+    // Mode d'emploi :
+    // argc et argv sont les valeurs parvenues de main
+
 //-------------------------------------------- Constructeurs - destructeur
 
     Traitement ( );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
 
     ~Traitement ( );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
 
 //------------------------------------------------------------------ PRIVE
 
 protected:
 //----------------------------------------------------- Méthodes protégées
-	
     void TraitementGeneral(string f);
+    // Mode d'emploi :
+    // f est le nom de fichier outilise pour la lecture des donnees
     
     void CreerFichierDot();
 
 //----------------------------------------------------- Attributs protégés
-    map <pair<string,string>,int> trajets;
+	map <pair<string,string>,int> trajets;
     map <string,int> hits;
     vector <pair<string, int>> top;
     Option option;
     bool erreur =false;
 };
-
-//-------------------------------- Autres définitions dépendantes de <Traitement>
-
 #endif // Traitement_H
-
